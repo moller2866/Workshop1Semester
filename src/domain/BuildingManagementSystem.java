d870af6fd14672f927153421ea463d7a3eca28f7
moller2866 <kaspermoeller.t@gmail.com>
2022-11-23 16:30:34 +0100

    added interface

--- src/domain/BuildingManagementSystem.java
+++ src/domain/BuildingManagementSystem.java
@@ -1,10 +1,8 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
-public class BuildingManagementSystem {
+public class BuildingManagementSystem implements IBuildingManagementSystem {
     List<Building> buildings = new ArrayList<>();
 
     public List<Building> getBuildings() {
@@ -30,11 +28,82 @@ public class BuildingManagementSystem {
         for (Building b : buildings) {
             String buildingStr =
                     "Building name: " + b.getName() +
-                    "\nUUID: " + b.getID().toString() +
-                    "\nSensors: " + b.getSensors() +
-                    "\nActuators: " + b.getActuators() + "\n";
+                            "\nUUID: " + b.getID().toString() +
+                            "\nSensors: " + b.getSensors() +
+                            "\nActuators: " + b.getActuators() + "\n";
             s = s + buildingStr;
         }
         return s;
+    }
+
+    @Override
+    public Map<UUID, String> getBuildingInformation() {
+        Map<UUID, String> map = new HashMap<>();
+        for (Building b : buildings) {
+            map.put(b.getID(), b.getName());
+        }
+        return map;
+    }
+
+    @Override
+    public Map<UUID, String> getSensorInformation(UUID buildingId) {
+        Map<UUID, String> map = new HashMap<>();
+        Building building = getBuilding(buildingId);
+        if (building != null) {
+            for (Sensor s : building.getSensors()) {
+                map.put(s.getID(), s.getName());
+            }
+        }
+        return map;
+    }
+
+    @Override
+    public Map<UUID, String> getActuatorInformation(UUID buildingId) {
+        Map<UUID, String> map = new HashMap<>();
+        Building building = getBuilding(buildingId);
+        if (building != null) {
+            for (Actuator s : building.getActuators()) {
+                map.put(s.getID(), s.getName());
+            }
+        }
+        return map;
+    }
+
+    private Building getBuilding(UUID buildingId) {
+        Building building = null;
+        for (Building b : buildings) {
+            if (b.getID() == buildingId) building = b;
+        }
+        return building;
+    }
+
+    @Override
+    public UUID addTemperatureSensor(UUID buildingId, String name) {
+        Building building = getBuilding(buildingId);
+        return building.addTemperatureSensor(name);
+    }
+
+    @Override
+    public UUID addCo2Sensor(UUID buildingId, String name) {
+        Building building = getBuilding(buildingId);
+        return building.addCO2Sensor(name);
+    }
+
+    @Override
+    public void removeSensor(UUID buildingId, UUID sensorId) {
+        Building building = getBuilding(buildingId);
+         building.removeSensor(sensorId);
+    }
+
+    @Override
+    public UUID addVentilationActuator(UUID buildingId, String name) {
+        Building building = getBuilding(buildingId);
+        return building.addVentilationActuator(name);
+    }
+
+    @Override
+    public void removeActuator(UUID buildingId, UUID actuatorId) {
+        Building building = getBuilding(buildingId);
+         building.removeActuator(actuatorId);
     }
 }
