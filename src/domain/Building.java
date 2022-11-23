e6957f70d4d830f72ff98fc28cbbfee19f05fb7f
jlars22 <jlars22@student.sdu.dk>
2022-11-23 15:08:46 +0100

    minor fixes

--- src/domain/Building.java
+++ src/domain/Building.java
@@ -1,5 +1,9 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Building extends Unit {
     private List<Sensor> sensorList = new ArrayList<Sensor>();
     private List<Actuator> actuatorList = new ArrayList<Actuator>();
